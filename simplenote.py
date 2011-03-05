81bedf0ca6f9fd95161f222362ff702ef04da6a1
Todd Eddy <vr@vrillusions.com>
2011-03-04 22:42:31 -0500

    Add initial login and index getting functions.

--- simplenote.py
+++ simplenote.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 # vim:ts=4:sw=4:ft=python:fileencoding=utf-8
-"""Getopts template
+"""Simplenote CLI
 
-When a script has many command line options, use this.
-
-TODO: perhaps better way to handle global variables, options, etc
+Command line interface to simplenote.
 
 """
 
 
 import urllib
 import urllib2
+import base64
+import sys
+import traceback
+from optparse import OptionParser
+from ConfigParser import ConfigParser
 
 # Make some effort to be backwards compatible with 2.5
 try: 
@@ -30,10 +33,61 @@ class Simplenote:
         self.password = password
     
     def login(self):
-        pass
+        # the login url is just api
+        url = 'https://simple-note.appspot.com/api/login'
+        data = 'email=' + self.email + '&password=' + self.password
+        data = base64.b64encode(data)
+        fh = urllib2.urlopen(url, data)
+        self.authtok = fh.read()
+        fh.close()
     
     def note(self):
         pass
     
     def delete(self):
         pass
+    
+    def index(self, length=100):
+        url = self.base_url + 'index'
+        data = 'length=' + str(length) + '&auth=' + self.authtok + '&email=' + self.email
+        # this is a get request so everything is sent in url
+        fh = urllib2.urlopen(url + '?' + data)
+        index = fh.read()
+        fh.close()
+        return(index)
+
+ 
+def main():
+    """The main function."""
+    parser = OptionParser(version='%prog v' + __version__)
+    parser.add_option('-c', '--config', default='config.ini',
+        help='Location of config file (default: %default)', metavar='FILE')
+    (options, args) = parser.parse_args() 
+ 
+    config = ConfigParser()
+    config.read(options.config)
+    email = config.get('simplenote', 'email')
+    password = config.get('simplenote', 'password')
+    
+    sn = Simplenote(email, password)
+    sn.login()
+    print sn.index(1)
+ 
+ 
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt, e:
+		# Ctrl-c
+		raise e
+	except SystemExit, e:
+		# sys.exit()
+		raise e
+	except Exception, e:
+		print "ERROR, UNEXPECTED EXCEPTION"
+		print str(e)
+		traceback.print_exc()
+		sys.exit(1)
+	else:
+		# Main function is done, exit cleanly
+		sys.exit(0)
